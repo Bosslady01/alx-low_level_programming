a887ccfe7edd0ec4c775e5fdcd7b652aa2cdd1b3
Bosslady01 <doyincoker123@gmail.com>
2022-09-08 19:23:24 -0700

    this prints with the printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main:this is the main entry of the code
  * return:should return a non-negative number
 */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
