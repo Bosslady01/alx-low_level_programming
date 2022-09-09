b62705fa7782b6280520d801e4de494b71d029ff
Bosslady01 <doyincoker123@gmail.com>
2022-09-08 19:09:47 -0700

    this prints with the printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main:this is the main entry point of the programme
+ * return:this should return a non-negative number at success and eof
+*/
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,%d");
+	return (0);
+}
