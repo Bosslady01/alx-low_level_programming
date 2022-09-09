809dba36463907cd940eea7656b8bf1f4c664e91
Bosslady01 <doyincoker123@gmail.com>
2022-09-08 19:40:17 -0700

    this prints with the printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main-this is the main entry of the code
- * return-should return a zero if no error,return non-zero if error
-*/
+ *  * main-entry point for the program.
+ *   *
+ *    * Return:0 if no errors, return non zero if error
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
