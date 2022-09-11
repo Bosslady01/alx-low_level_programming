195fb759ce34632e3a56e471270eae689c0ba4c0
Bosslady01 <doyincoker123@gmail.com>
2022-09-11 01:11:20 +0100

    this file prints the if statement of a given variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/* ./betty-style.pl main.c */
+/* git./betty-style.pl main.c */
 int main(void)
 {
 	int n;
@@ -13,10 +13,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
     if (n > 0)
-        printf("%d  is  positive\n", n);
+        printf("%d is positive\n", n);
     else if (n == 0)
-        printf("%d  is  zero\n", n);
+        printf("%d is zero\n", n);
     else
-        printf("%d  is  negative\n", n);
+        printf("%d is negative\n", n);
     return (0);
 }
