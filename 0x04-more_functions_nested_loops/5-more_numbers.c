28d06bf775bb821e812de2d28343fd64947f24d7
Bosslady01 <doyincoker123@gmail.com>
2022-09-16 05:39:36 +0100

    a function that prints the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,17 @@
 
 void more_numbers(void)
 {
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i <= 14)
+			if (i > 9)
 				_putchar((i / 10) + '0');
 			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
+	}
 }
 
