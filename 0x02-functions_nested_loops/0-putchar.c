0e538e71c1796ad85627f0c20833f883dce7270d
Bosslady01 <doyincoker123@gmail.com>
2022-09-16 10:16:39 +0100

    nestted

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,14 @@
-#include "main.h"
+#ifndef main.h
+#define main .h
 
+int_putchar(char c);
+#endif
 /**
-*main - Entry point
-*
-*Return: 0 nothing, return void
-*/
-
-int main(void)
-
+ *	* main - this is the main entry point of this program,
+ *	* Return: 0 at the end of the program
+ */
+int main.h(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	printf("_putChar,\n");
 	return (0);
 }
-
