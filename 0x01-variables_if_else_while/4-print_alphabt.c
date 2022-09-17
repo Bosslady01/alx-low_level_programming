a7c82a862dec82ada9f80b45bd7c27c970ba0210
Bosslady01 <doyincoker123@gmail.com>
2022-09-17 20:52:27 +0100

    command

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                if (ch != 'e' && ch != 'q')
-                {
-                        putchar(ch);
-                }
-                ch++;
-        }
-        putchar('\n');
+	while (ch <= 'z')
+	{
+	if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
