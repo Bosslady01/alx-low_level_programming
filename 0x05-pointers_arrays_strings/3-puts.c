33984c531ea60853ae4e5596df576a5303fff340
Bosslady01 <doyincoker123@gmail.com>
2022-09-19 22:48:39 +0100

    a function that prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -15,10 +15,10 @@ void _puts(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	_puts(str[i]);
+	_puts (str[i]);
 
 	}
-	_puts('\n');
+	_puts ('\n');
 
 }
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file containing prototypes
- * void prototype (void)
- * int prototype (int)
- */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
