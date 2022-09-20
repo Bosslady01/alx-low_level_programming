c82ac4611364f15bd5be9a9713095fcacb708aae
Bosslady01 <doyincoker123@gmail.com>
2022-09-20 01:03:23 +0100

    a function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcpy - copies  src to  dest.
- * @dest:char
- * @src:char
- * Return:char
+ * print_array - display array
+ * @a:integer
+ * @n:integer
+ *Return:void
  */
-char *_strcpy(char *dest, char *src)
+void print_array(int *a, int n)
 {
 int i;
-
-i = 0;
-	while (src[i])
+for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
-		i++;
+	printf("%d", a[i]);
+		if (i < (n - 1))
+		{
+		printf(", ");
+		}
 	}
-dest[i] = '\0';
-return (dest);
+	printf("\n");
 }
+
