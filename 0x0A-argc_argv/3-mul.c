6616fe5e9d21782d0403ee6a6dba088ed1302958
Bosslady01 <doyincoker123@gmail.com>
2022-10-01 04:21:50 +0100

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,9 +2,9 @@
 #include "main.h"
 /**
  * main - Program that multiplies two numbers.
+ * atoi is a function that converts a string into an int
  * @argc: This is the argument count
  * @argv: This is the argument vector
- *
  * Return: 0;
  */
 int main(int argc, char *argv[])
